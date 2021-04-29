ec55cb9b1aa14d67f4146e3712c359fcb06d9b2e
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-04-29 10:43:48 +0530

    Update end.js

--- end.js
+++ end.js
@@ -3,15 +3,29 @@ const savScoreBtn=document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore= localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORE=5;
+//console.log(highScores);
+
 finalScore.innerText=mostRecentScore;
-
-
 username.addEventListener('keyup',()=>{
     //console.log(username.value);
-    savScoreBtn.disabled =!username.nodeValue;
+    saveScoreBtn.disabled =!username.value;
 });
 
-saveHighScore = (e) =>{
-    console.log("Clicked to save");
+saveHighScore= function (e) {
+    //console.log("Clicked to save");
     e.preventDefault();
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+    highScores.push(score);
+    highScores.sort((a,b) => b.score-a.score );
+    highScores.splice(5);
+
+    localStorage.setItem('highScores',JSON.stringify(highScores));
+    window.location.assign('/');
+    //console.log(highScores);
 }
